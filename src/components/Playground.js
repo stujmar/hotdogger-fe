8facd8209d03b661a0fccb36ec6930ad1dac6557
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-13 22:02:57 -0600

    made button with attached clickhandler method

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -3,11 +3,13 @@ import React, {useState} from "react";
 
 const Playground = () => {
     const [counter, setCounter] = useState(0);
+    const handleClick = () => {setCounter(counter+1)}
     return (
         <div>This is the Playground
         {counter}
+        <button onClick={handleClick}>Click me</button>
         </div>
-    )a
+    )
 }
 
 export default Playground;
