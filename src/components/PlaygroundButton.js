c012b6bcb0cacc71d2fcf29eb57bd8515d75e105
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-15 23:24:08 -0600

    made a ternary in button to deal with dispay of negative numbers

--- src/components/PlaygroundButton.js
+++ src/components/PlaygroundButton.js
@@ -3,10 +3,11 @@ import React from "react"
 const PlaygroundButton = (props) => {
 
     const handleClick = () => props.onClick(props.increment);
-
+    let prefix = props.increment > 0 ? "+" : "";
+    let display = `${prefix}${props.increment}`;
     return (
         <button onClick={handleClick}>
-            +{props.increment}
+            {display}
         </button>
     )
 }
