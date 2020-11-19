5d9f032274bdec6cede90144fa4a745341d7505e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-18 20:48:58 -0600

    made test component

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Button from "./Button.js"
 import PlaygroundButton from "./PlaygroundButton.js";
+import Test from "./Test.js"
 
 const Playground = ({label, amount}) => {
     const [counter, setCounter] = useState(0);
@@ -20,22 +21,29 @@ const Playground = ({label, amount}) => {
         , 1000);
     }
     
+    const logger = () => {
+        console.log('button press');
+    }
+
     slender();
 
     return (
         <div>{`${label}`}
-            {counter}
+            <Test name={"Liz"}/>
+            <Test name={"Stu"}/>
+            <Test name={"Kitty"}/>
+            {/* {counter}
             <Button />
             <button onClick={handleClick}>Click me</button>
             {num}
             <input></input>
-            <button>CLICK ME</button>
+            <button onClick={logger}>CLICK ME</button>
             <PlaygroundButton onClick={increment} increment={1}/>
             <PlaygroundButton onClick={increment} increment={5}/>
             <PlaygroundButton onClick={increment} increment={10}/>
             <PlaygroundButton onClick={increment} increment={25}/>
             <PlaygroundButton onClick={increment} increment={100}/>
-            <PlaygroundButton onClick={increment} increment={-100}/>
+            <PlaygroundButton onClick={increment} increment={-100}/> */}
         </div>
     )
 }
---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = ({name}) => {
+
+    let currentName = name
+    
+    return (
+        <div>{`HELLO ${currentName}`}</div>
+    )
+}
+
+export default Test;
