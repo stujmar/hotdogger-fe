76839ddd591eb7799a508336a186a0cbcd19f588
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-19 23:46:18 -0600

    added this logs and wrapped loop in a function

--- src/components/Test.js
+++ src/components/Test.js
@@ -5,11 +5,14 @@ const Test = ({name}) => {
     let currentName = name + "!";
     {{{}}};
 
-    for (let i = 0; i<= 10; i++) {
-        console.log(`In scope i: ${i}`);
+    const looper = () => {
+        for (let i = 0; i<= 10; i++) {
+            console.log(`In scope i: ${i}`, this);
+        }
     }
-    console.log('out of scope');
-    
+    looper();
+    console.log('out of scope', this);
+
     return (
         <div>{`HELLO ${currentName}`}</div>
     )
