40425c0b4e07deecc318b01a5d8ab00fe22e5e7f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-19 23:34:29 -0600

    testing logging loop in react

--- src/components/Test.js
+++ src/components/Test.js
@@ -5,6 +5,10 @@ const Test = ({name}) => {
     let currentName = name + "!";
     {{{}}};
 
+    for (let i = 0; i<= 10; i++) {
+        console.log(`In scope i: ${i}`);
+    }
+
     return (
         <div>{`HELLO ${currentName}`}</div>
     )
