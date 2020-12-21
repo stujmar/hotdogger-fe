7e665ae2ddf4ff543ae3da4cc0900b775114762f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-20 21:50:56 -0600

    made some major strides in why my dumb app wasn't working

--- src/components/TickTimer.js
+++ src/components/TickTimer.js
@@ -5,22 +5,23 @@ const TickTimer = () => {
 
     let countDisplay;
 
-    const {count, setCount} = useState(5);
-    const {nums, setNums} = useState([1,2,3]);
-    console.log("Hello World")
+    const [count, setCount] = useState(5);
+    const [nums, setNums] = useState([1,2,3]);
+    console.log("Hello from Loose is in the Functional Component")
 
     useEffect(() => {
-        countDisplay = count;
+        console.log("I'm in a useEffect")
     }, []);
 
     const clickHandler = () => {
         console.log("hello world");
+        setCount(6);
     }
 
     return (
         <div style={{background: "white", height: "100px", color: "black"}}>
-            {`${countDisplay}`}
             {count}
+            {countDisplay}
             {nums}
             <button onClick={clickHandler}>Click Me</button>
         </div>
