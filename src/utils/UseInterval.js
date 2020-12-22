fc2b7fd3973da4ad0cef1906014b66c99bdd46b3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-21 21:54:26 -0600

    built out UseInterval logic from Dan Abramov

--- src/utils/UseInterval.js
+++ src/utils/UseInterval.js
@@ -1,5 +1,23 @@
-const UseInterval = (x, y) => {
-    return x + y;
+import { useEffect, useRef } from 'react'; 
+
+const UseInterval = (callback, delay) => {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback])
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
 }
 
 export default UseInterval
