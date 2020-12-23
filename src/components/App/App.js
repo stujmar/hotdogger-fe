6b5c0d1bcc369ff994f19284d329e75438e9989c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-22 20:05:13 -0600

    cleaned up app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,38 +13,10 @@ import UpgradePanel from '../UpgradePanel';
 import BadgePanel from '../BadgePanel';
 import NoMatch from '../NoMatch';
 import LoginModal from '../LoginModal/LoginModal';
-import Playground from '../Playground';
-import TickTimer from '../TickTimer';
 
 function App() {
 
   const [loginModal, setLoginModal] = useState(true)
-  let [count, setCount] = useState(0);
-  let [click, setClick] = useState(0);
-  let [timerRun, setTimerRun] = useState(false);
-  let myCounter;
-
-  const startHandler = () => {
-    setTimerRun(true);
-    setClick(click + 1);
-  };
-
-  const updateDogs = () => {
-    if(timerRun){
-      setCount(count => count + 1)
-    }
-  }
-  
-  useEffect(() => {
-      myCounter = setInterval(() => {
-      updateDogs();
-    },1000)
-  },[timerRun]);
-  
-  const stopHandler = () => {
-    setTimerRun(false);
-    clearInterval(myCounter);
-  }
 
   const handleLoginModal = () => {
     console.log('we got to the onClose handler');
@@ -54,14 +26,10 @@ function App() {
   return (
     <Router>
       {/* <Playground label="Playground"/> */}
-      <TickTimer />
-      {/* <div className={rootStyle}>
+      {/* <TickTimer /> */}
+      <div className={rootStyle}>
         <LoginModal status={loginModal} onClose={handleLoginModal} />
         <div className={bg}>
-          {click}
-          <button onClick={startHandler}>Start</button>
-          <button onClick={stopHandler}>Stop</button>
-          {count}
           <Header />
           <div className={wrapper}> 
           <Switch>
@@ -73,7 +41,7 @@ function App() {
           </div>
           <Footer />
         </div>
-      </div> */}
+      </div>
     </Router>
   );
 }
