a8a32e62e86ef8e03e5425733190d079ed9f3afd
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-22 20:15:45 -0600

    made a structure for the new menu bar with logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, {Fragment } from 'react';
 import { useLocation } from 'react-router-dom';
-import { headerStyle, superTitle, title, buttonBar } from '../Header/Header.module.css';
+import { headerStyle, superTitle, title, buttonBar, grayBar, menuBar, menuParent, logoStyle } from '../Header/Header.module.css';
 import Button from '../Button';
 import InfoPanel from '../InfoPanel/InfoPanel';
 
@@ -35,6 +35,11 @@ const Header = () => {
     
         return (
             <Fragment>
+                <div className={menuParent}>
+                    <div className={logoStyle}></div>
+                    <div className={grayBar}></div>
+                    <div className={menuBar}></div>
+                </div>
                 <div className={headerStyle}>
                     <div className={superTitle}>the original</div>
                     <h1 className={title}>HotDogger</h1>
