e171d6e412f9217fdc1c0bf94f0129542f72c03b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-25 16:45:46 -0600

    turned off old navigation and changed styling on new one

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React, {Fragment } from 'react';
 import { useLocation } from 'react-router-dom';
-import { buttonBar, cart, grayBar, grayButton, menuBar, menuParent, logoStyle, darker, background, menuButton } from '../Header/Header.module.css';
+import { activeButton, buttonBar, cart, grayBar, grayButton, menuBar, menuParent, logoStyle, darker, background, menuButton } from '../Header/Header.module.css';
 import Button from '../Button';
 import InfoPanel from '../InfoPanel/InfoPanel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import shoppingCart from '../../resources/shopping_cart.svg'
+import { useHistory } from "react-router-dom";
 
 const Header = () => {
-
+    const history = useHistory();
     let location = useLocation();
     let upgradeStyle, homeStyle, badgeStyle = {};
     let borderLeft = '2px solid brick';
@@ -34,7 +35,10 @@ const Header = () => {
     location.pathname === '/' ? homeStyle = activeTab : homeStyle = passiveTab; 
     location.pathname === '/badges' ? badgeStyle = activeTab : badgeStyle = passiveTab;
 
-    
+    homeStyle = location.pathname === '/' ? `${menuButton} ${activeButton}` : `${menuButton}`;
+    badgeStyle = location.pathname === '/badges' ? `${menuButton} ${activeButton}` : `${menuButton}`;
+    upgradeStyle = location.pathname === '/upgrades' ? `${menuButton} ${activeButton}` : `${menuButton}`;
+
         return (
             <Fragment>
                 <div className={menuParent}>
@@ -51,20 +55,23 @@ const Header = () => {
                     <div className={menuBar}>
                         <button onClick={() => {
                             console.log("Let's go home.");
-                        }} className={`${menuButton}`}>HOME</button>
+                            history.push("/"); 
+                        }} className={homeStyle}>HOME</button>
                         <button onClick={() => {
-                            console.log("Let's Upgrade.")
-                        }} className={`${menuButton}`}>UPGRADES</button>
+                            console.log("Let's Upgrade.");
+                            history.push("/upgrades"); 
+                        }} className={upgradeStyle}>UPGRADES</button>
                         <button onClick={() => {
-                            console.log("Let's look at Badges.")
-                        }} className={`${menuButton}`}>BADGES</button>
+                            console.log("Let's look at Badges.");
+                            history.push("/badges"); 
+                        }} className={`${badgeStyle}`}>BADGES</button>
                     </div>
                 </div>
-                    <div className={buttonBar}>
+                    {/* <div className={buttonBar}>
                             <Button id={0} style={upgradeStyle} path='/upgrades' content='Upgrades'/>
                             <Button id={1} style={homeStyle} path='/' content='Home'/>
                             <Button id={2} style={badgeStyle} path='/badges' content='Badges'/>
-                    </div>
+                    </div> */}
             </Fragment>
         )
     
