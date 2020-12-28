6366ec2bd84742a766e3999a4b78fa4214f37ca0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-28 16:31:41 -0600

    added login link to homepage and media queries

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,13 @@
 import React, {Fragment} from 'react';
-import  { background } from './Home.module.css'
+import  { background, title } from './Home.module.css'
 
 const Home = () => {
     return (
         <Fragment>
             <div className={`${background}`}>
-                <div className="title">Welcome to Hot Dogger</div>
+                <div className={title}>Welcome to Hot Dogger</div>
                 <button>Get Started!</button>
-                <p>login to track your progress!</p>
+                <p>or <a href="#">login</a> to track your progress!</p>
             </div>
         </Fragment>
     )
