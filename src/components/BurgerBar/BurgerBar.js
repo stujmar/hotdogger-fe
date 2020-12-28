abe478e0771dc960d503fe0f6574ebe10c675022
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-27 20:21:33 -0600

    got hamburger menu to toggle

--- src/components/BurgerBar/BurgerBar.js
+++ src/components/BurgerBar/BurgerBar.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { burgerBar, hamburger, mobileLogo, on, off } from './BurgerBar.module.css'
 
-const BurgerBar = (toggle) => {
+const BurgerBar = ({ toggle, onClick }) => {
 
-    let menuStyle = toggle ? `${hamburger} ${on}` : `${hamburger} ${off}`
+    const [menuStyle, setMenuStyle] = useState(`${hamburger} ${on}`);
+
+    if (toggle && menuStyle == `${hamburger} ${off}`) {
+        console.log("going on");
+        setMenuStyle(`${hamburger} ${on}`); 
+    } else if (!toggle && menuStyle == `${hamburger} ${on}`) {
+        console.log("going off");
+        setMenuStyle(`${hamburger} ${off}`);
+    }
+        
+    const toggleHandler = () => {
+        onClick();
+    }
 
     return (
-        <div className={burgerBar}>
+        <button onClick={toggleHandler} className={burgerBar}>
             <div className={mobileLogo}></div>
         <div className={menuStyle}></div>
-        </div>
+        </button>
     )
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,8 @@
-import React, {Fragment } from 'react';
+import React, {Fragment, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { activeButton, buttonBar, cart, grayBar, grayButton, menuBar, menuParent, logoStyle, darker, background, menuButton } from '../Header/Header.module.css';
 import Button from '../Button';
 import InfoPanel from '../InfoPanel/InfoPanel';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import shoppingCart from '../../resources/shopping_cart.svg'
 import { useHistory } from "react-router-dom";
 import BurgerBar from '../BurgerBar/BurgerBar';
@@ -12,37 +11,24 @@ const Header = () => {
     const history = useHistory();
     let location = useLocation();
     let upgradeStyle, homeStyle, badgeStyle = {};
-    let borderLeft = '2px solid brick';
-    let borderRight = '2px solid black';
-    
-    location.pathname === '/upgrades' ? borderLeft = '0px solid black' : borderLeft = '2px solid black';
-    location.pathname === '/badges' ? borderRight = '0px solid black' : borderRight = '2px solid black';
-    
-    let activeTab = {
-        backgroundColor: "yellowgreen",
-        color: 'rgb(255, 94, 0)',
-        borderRight: `${borderRight}`,
-        borderLeft: `${borderLeft}`,
-        borderBottom: "0px solid",
-    
-    }
-    
-    let passiveTab = {
-        backgroundColor: "rgb(255, 94, 0)",
-        color: '#FFDB3A',  
-    }
 
-    location.pathname === '/upgrades' ? upgradeStyle = activeTab : upgradeStyle = passiveTab;
-    location.pathname === '/' ? homeStyle = activeTab : homeStyle = passiveTab; 
-    location.pathname === '/badges' ? badgeStyle = activeTab : badgeStyle = passiveTab;
+    const [mobileMenu, setMobileMenu] = useState(false); 
+
+    // location.pathname === '/upgrades' ? upgradeStyle = activeTab : upgradeStyle = passiveTab;
+    // location.pathname === '/' ? homeStyle = activeTab : homeStyle = passiveTab; 
+    // location.pathname === '/badges' ? badgeStyle = activeTab : badgeStyle = passiveTab;
 
     homeStyle = location.pathname === '/' ? `${menuButton} ${activeButton}` : `${menuButton}`;
     badgeStyle = location.pathname === '/badges' ? `${menuButton} ${activeButton}` : `${menuButton}`;
     upgradeStyle = location.pathname === '/upgrades' ? `${menuButton} ${activeButton}` : `${menuButton}`;
 
+    const menuToggler = () => {
+        setMobileMenu(!mobileMenu);
+    }
+
         return (
             <Fragment>
-            <BurgerBar toggle={true}/>
+            <BurgerBar toggle={mobileMenu} onClick={menuToggler}/>
                 <div className={menuParent}>
                     <button className={logoStyle}></button>
                     <div className={grayBar}>
