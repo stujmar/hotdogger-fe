d769353f29b0569ca914415cdd57697897097ab0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-27 18:25:41 -0600

    made BurgerBar file and folder structure

---
+++ src/components/BurgerBar/BurgerBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { burgerBar } from './BurgerBar.module.css'
+
+const BurgerBar = () => {
+    return (
+        <div className={burgerBar}>
+
+        </div>
+    )
+}
+
+export default BurgerBar
