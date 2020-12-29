8d579cf36a129c675261f5cbf662d4aede11e71b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-29 14:57:23 -0600

    got dog count from app state to infopanel

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { infoBody, infoWrap, infoItem, dogCount } from './InfoPanel.module.css';
 
-const InfoPanel = () => {
+const InfoPanel = ({dogs}) => {
 
     return (
         <div className={infoBody}>
             <div className={infoWrap}>
-                <div className={dogCount}>Hotdogs: 100</div>
+                <div className={dogCount}>Hotdogs: {dogs}</div>
                 <div className={infoItem}>Customers: 100</div>
                 <div className={infoItem}>Total Clicks: 100</div>
                 <div className={infoItem}>Click Power: 100</div>
