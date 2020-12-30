e3873420e3ef7dbe4328af7fbae41c84fdf34f1b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-30 16:28:38 -0600

    brought vanilla gamestate functions into a util file

---
+++ src/utils/GameState.js
@@ -0,0 +1,11 @@
+const SaveGame = (gameObject) => {
+    console.log('Game saved.');
+};
+
+const LoadGame = (userId) => {
+    console.log('Game loaded.');
+};
+
+const ResetGame = (userId) => {
+    console.log('Game reset.');
+};
