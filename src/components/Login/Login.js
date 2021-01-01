89dbb68051a94a8d22e2c6f46af7580edbeffd19
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-31 18:45:00 -0600

    added loginToggle button to login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,8 @@ import React, { Fragment } from 'react'
 const Login = () => {
     return (
         <Fragment>
-            <p>This is the Login Page</p>
+            <p>This is the Login Page</p><br/>
+            <button>Login Toggle</button>
         </Fragment>
     )
 }
