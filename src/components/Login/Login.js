7b68704cec0871c00159e0af44f86601adbbe61f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-12-31 18:50:15 -0600

    added a handler for the toggle login button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react'
 import { loginBody, loginButton } from './Login.module.css'
 
+const handleLoginToggle = () => {
+    console.log("toggle login");
+}
+
 const Login = () => {
     return (
         <div className={loginBody}>
             <p>This is the Login Page</p><br/>
-            <button className={loginButton}>Login Toggle</button>
+            <button className={loginButton} onClick={handleLoginToggle}>Login Toggle</button>
         </div>
     )
 }
