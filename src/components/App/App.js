4690118bbede76881d14e1664e59b4c67c636cf1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-01 19:52:42 -0600

    made App level method to toggle login state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -38,6 +38,10 @@ const [game, setGame] = useState({
     setLoginModal(false);
   }
 
+  const handleLogin = () => {
+    setLogin(!login);
+  }
+
   return (
     <Router>
       {/* <Playground label="Playground"/> */}
