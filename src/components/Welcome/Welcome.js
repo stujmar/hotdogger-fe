e3038ebbf0707659c388aa842588afc821a7af77
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-05 22:42:58 -0600

    brought in JSX of Welcome from Home

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
-import { styles } from './Welcome.module.css'
+import { background, title, getStarted, loginButton } from './Welcome.module.css'
 
 
 const Welcome = () => {
+    const history = useHistory();
+
+    const clickHandler = () => {
+        history.push("/game");
+    }
+
+    const loginHandler = () => {
+        history.push("/login");
+    }
+
     return (
-        <div className={styles}></div>
+            <div className={`${background}`}>
+                <div className={title}>Welcome to Hot Dogger</div>
+                <button className={getStarted} onClick={clickHandler}>Get Started!</button>
+                <p>or <button className={loginButton} onClick={loginHandler}>login</button> to track your progress!</p>
+            </div>
     )
+}
 }
 
 export default Welcome
