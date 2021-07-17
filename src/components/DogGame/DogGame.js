8091674e1c4e44595351d6cb566383d51f5aa615
Stuart Marsh <stujmar@gmail.com>
2021-07-17 10:27:10 -0500

    [sm] built outcompononet structure for DogGame and linked it to its module.css

---
+++ src/components/DogGame/DogGame.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { dogBackground } from './DogGame.module.css';
+
+const DogGame = () => {
+    return (<React.Fragment>
+        <div className={dogBackground}></div>
+    </React.Fragment>)
+}
+
+export default DogGame;
