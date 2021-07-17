f93f2fec0989236e0755e6a490fc3469251a86e0
Stuart Marsh <stujmar@gmail.com>
2021-07-17 10:18:46 -0500

    [sm] eliminated all tree related reducers gtom the gameSlice.js

--- src/redux/gameSlice.js
+++ src/redux/gameSlice.js
@@ -18,32 +18,6 @@ export const gameSlice = createSlice({
         setMode: (state, action) => {
             state.mode = action.payload;
         },
-        addTree: (state, action) => {
-            state.trees = state.trees.concat(action.payload)
-        },
-        removeTree: (state, action) => {
-            state.trees = state.trees.filter(tree => {
-               return tree.id !== action.payload;
-            })        
-        },
-        resetTrees: (state) => {
-            state.trees = [];
-        },
-        ageTrees: (state) => {
-            state.trees = state.trees.map(tree => {
-                return tree.age < 80 ? {...tree, age: tree.age + 1} : tree
-            })
-        },
-        growTrees: (state, action) => {
-            state.trees = state.trees.map(tree => {
-                return tree.growth.length < 40 && tree.age < 80 ? {...tree, growth: tree.growth.concat(action.payload)} : tree;
-            })
-        },
-        removeTreeById: (state, action) => {
-            state.trees = state.trees.filter(tree => {
-                return tree.id !== action.payload;
-            })
-        },
         updateSeeds: (state, action) => {
             state.resources.seeds = state.resources.seeds + action.payload;
         },
