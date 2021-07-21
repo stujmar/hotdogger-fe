41c707a5b36e55e442739c42ba79143b7f16d399
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-20 19:29:35 -0500

    [sm] drastically overhauled the home screen probably not for the better

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory, Redirect } from "react-router-dom";
 
-import  { background, getStarted, loginButton, title } from './Home.module.css'
+import  { background, getStarted, loginButton, title, flexHolder, cta } from './Home.module.css'
 
 const Home = ({isLoggedIn, onClick}) => {
     const history = useHistory();
@@ -15,11 +15,14 @@ const Home = ({isLoggedIn, onClick}) => {
     }
 
     return (
-            <div className={`${background}`}>
-                { isLoggedIn ? null : <Redirect to="/welcome"/>}
+            <div className={flexHolder}>
                 <div className={title}>Welcome to Hot Dogger</div>
-                <button className={getStarted} onClick={clickHandler}>Get Started!</button>
-                <p>or <button className={loginButton} onClick={dogHandler}>DOG</button> click that dog!</p>
+                <div className={`${background}`}></div>
+                <div className={cta}>
+                    { isLoggedIn ? null : <Redirect to="/welcome"/>}
+                    <button className={getStarted} onClick={clickHandler}>Get Started!</button>
+                    <p>or <button className={loginButton} onClick={dogHandler}>DOG</button> click that dog!</p>
+                </div>
             </div>
     )
 }
