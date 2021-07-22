4dc26497ec40430b5959dfb0619e55e2df256222
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-21 20:13:02 -0500

    [sm] made a wrapper class around my drawing dog layer to be a button and parent of the dog and toppings

--- src/components/DogGame/DogGame.js
+++ src/components/DogGame/DogGame.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { dogBackground, dogImage } from './DogGame.module.css';
+import { dogBackground, dogImage, gameWrapper } from './DogGame.module.css';
 
 const DogGame = () => {
     const [ onions, setOnions ] = useState([]);
 
     return (<React.Fragment>
         <div className={dogBackground}>
-            <div className={dogImage}>
-                
+            <div className={gameWrapper}>
+
+                <div className={dogImage}></div>
             </div>
         </div>
     </React.Fragment>)
