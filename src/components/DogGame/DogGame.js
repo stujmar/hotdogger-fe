2c1e24de371cbf6c20d616e97f98abb613823a73
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-21 20:53:00 -0500

    [sm] added a class and div for a plate and added made the dog a little bigger

--- src/components/DogGame/DogGame.js
+++ src/components/DogGame/DogGame.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import { dogBackground, dogImage, gameWrapper } from './DogGame.module.css';
+import { dogBackground, dogImage, gameWrapper, plate } from './DogGame.module.css';
 
 const DogGame = () => {
     const [ onions, setOnions ] = useState([]);
 
     return (<React.Fragment>
         <div className={dogBackground}>
+        <div className={plate}>
+
             <div className={gameWrapper}>
 
                 <div className={dogImage}></div>
             </div>
+        </div>
         </div>
     </React.Fragment>)
 }
