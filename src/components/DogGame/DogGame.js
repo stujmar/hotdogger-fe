9ea8e28daf6adb6d227aa3960d658f6799244d85
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-21 21:02:23 -0500

    [sm] added the structure to the dog page for left and right button panel sections

--- src/components/DogGame/DogGame.js
+++ src/components/DogGame/DogGame.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { dogBackground, dogImage, gameWrapper, plate } from './DogGame.module.css';
+import { background, dogImage, gameWrapper, plate, flexParent, leftSection, rightSection } from './DogGame.module.css';
 
 const DogGame = () => {
     const [ onions, setOnions ] = useState([]);
 
     return (<React.Fragment>
-        <div className={dogBackground}>
-        <div className={plate}>
-
-            <div className={gameWrapper}>
-
-                <div className={dogImage}></div>
+        <div className={background}>
+        <div className={flexParent}>
+            <div className={leftSection}></div>
+            <div className={plate}>
+                <div className={gameWrapper}>
+                    <div className={dogImage}></div>
+                </div>
             </div>
+            <div className={rightSection}></div>
         </div>
         </div>
     </React.Fragment>)
