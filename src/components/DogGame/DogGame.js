3e3446539b3f9b31e025d76ce3d613c7dad3cc6b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-21 20:10:18 -0500

    [sm] added onions and state to the DogGame component

--- src/components/DogGame/DogGame.js
+++ src/components/DogGame/DogGame.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { dogBackground, dogImage } from './DogGame.module.css';
 
 const DogGame = () => {
+    const [ onions, setOnions ] = useState([]);
+
     return (<React.Fragment>
         <div className={dogBackground}>
             <div className={dogImage}>
