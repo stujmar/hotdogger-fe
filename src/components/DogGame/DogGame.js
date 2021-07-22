7e35c470c1399bac3ad0fce23f5c052bf4fc07f6
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-21 20:04:22 -0500

    [sm] added hotdog art to DogGame component

--- src/components/DogGame/DogGame.js
+++ src/components/DogGame/DogGame.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { dogBackground } from './DogGame.module.css';
+import { dogBackground, dogImage } from './DogGame.module.css';
 
 const DogGame = () => {
     return (<React.Fragment>
-        <div className={dogBackground}>Put game here.</div>
+        <div className={dogBackground}>
+            <div className={dogImage}>
+                
+            </div>
+        </div>
     </React.Fragment>)
 }
 
