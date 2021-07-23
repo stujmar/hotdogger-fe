fe04291b8ee763b92e5b2888f553e5512ece3216
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-22 21:04:09 -0500

    [sm] got info panel rendering dog count from redux state

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectGameStats } from '../../redux/gameSlice';
 import { infoBody, infoWrap, infoItem, dogCount } from './InfoPanel.module.css';
 
 const InfoPanel = ({dogs}) => {
+    
 
     return (
         <div className={infoBody}>
-            <div className={dogCount}>Hotdogs: {dogs}</div>
+            <div className={dogCount}>Hotdogs: {useSelector(selectGameStats).hotdogs}</div>
             <div className={infoWrap}>
                 <div className={infoItem}>Customers: 100</div>
                 <div className={infoItem}>Total Clicks: 100</div>
