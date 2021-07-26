6e8c788c6decba57b02895e50d8165ef1fcad3bc
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-25 21:43:57 -0500

    [sm] got infopanel sitck to the bottom and taking 100% width and everything stacking correctly

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectGameStats } from '../../redux/gameSlice';
-import { infoBody, infoWrap, infoItem, dogCount } from './InfoPanel.module.css';
+import { infoBody, infoWrap, infoItem, dogCount, footer } from './InfoPanel.module.css';
 
 const InfoPanel = ({dogs}) => {
     
 
     return (
-        <div className={infoBody}>
-            <div className={dogCount}>Hotdogs: {useSelector(selectGameStats).hotdogs}</div>
-            <div className={infoWrap}>
-                <div className={infoItem}>Customers: 100</div>
-                <div className={infoItem}>Total Clicks: 100</div>
-                <div className={infoItem}>Click Power: 100</div>
-                <div className={infoItem}>Dogs Per Second: 100</div>
+        <footer className={footer}>
+            <div className={infoBody}>
+                <div className={dogCount}>Hotdogs: {useSelector(selectGameStats).hotdogs}</div>
+                <div className={infoWrap}>
+                    <div className={infoItem}>Customers: 100</div>
+                    <div className={infoItem}>Total Clicks: 100</div>
+                    <div className={infoItem}>Click Power: 100</div>
+                    <div className={infoItem}>Dogs Per Second: 100</div>
+                </div>
             </div>
-        </div>
+        </footer>
     )
 }
 
