8f53acd5de97875f6b9b408e78aa0dcf64eb94bb
Stuart Marsh <stujmar@gmail.com>
2021-10-15 21:33:07 -0500

    [sm] whoops forgot to save the Home.js new class names working now

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory, Redirect } from "react-router-dom";
 
-import  { background, getStarted, loginButton, title, flexHolder, cta } from './Home.module.css'
+import  { background, getStarted, dogButton, title, flexHolder, cta } from './Home.module.css'
 
 const Home = ({isLoggedIn, onClick}) => {
     const history = useHistory();
@@ -21,7 +21,7 @@ const Home = ({isLoggedIn, onClick}) => {
                 <div className={cta}>
                     { isLoggedIn ? null : <Redirect to="/welcome"/>}
                     <button className={getStarted} onClick={clickHandler}>Get Started!</button>
-                    <p>or <button className={loginButton} onClick={dogHandler}>DOG</button> click that dog!</p>
+                    <p>or <button className={dogButton} onClick={dogHandler}>DOG</button> click that dog!</p>
                 </div>
             </div>
     )
