64e5411ba5c7da91b876918b0d7f42aa77ae2e39
Stuart Marsh <stujmar@gmail.com>
2021-10-16 21:05:59 -0500

    [sm] trying an absolute import from burgerbar to get the mobile image working again but with accessibility

--- src/components/BurgerBar/BurgerBar.js
+++ src/components/BurgerBar/BurgerBar.js
@@ -52,7 +52,7 @@ const BurgerBar = ({ toggle, onClick, isLoggedIn }) => {
                 <button className={`${cart}`}/>
             </div>
         </div>
-            <a href="/game" className={mobileLogo}><img src="../../resources/logo_mobile.png" alt="hotdogger logo"></img></a>
+            <a href="/game" className={mobileLogo}><img src="images/logo_mobile.png" alt="hotdogger logo"></img></a>
         <button onClick={toggleHandler} className={burgerBar}>
         <div className={menuStyle}></div>
         </button>
